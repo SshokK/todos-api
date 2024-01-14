30e77bdfa7da7425117b85e2f5d0f7dbf648b0ca
Grudinskiy <aleksandr.grudinskiy@compliance.ai>
2024-01-14 16:24:25 +0300

    Remove nest devtools

--- src/app/module/app.module.ts
+++ src/app/module/app.module.ts
@@ -1,15 +1,9 @@
 import * as nestCommon from '@nestjs/common';
 import * as providers from '../../providers';
 import * as repos from '../../repos';
-import * as nestDevtools from '@nestjs/devtools-integration';
-import * as globalConstants from '../../constants/global.constants';
 
 @nestCommon.Module({
   imports: [
-    nestDevtools.DevtoolsModule.register({
-      http:
-        globalConstants.ENV.NODE_ENV !== globalConstants.NODE_ENVS.PRODUCTION,
-    }),
     providers.MongoModule,
     repos.TodoModule,
   ],
